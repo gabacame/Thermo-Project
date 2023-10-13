972f0b60e9f47ba501050f9ffc040674cf736342
gabacame <ga.bacame@gmail.com>
2023-10-13 13:48:47 -0700

    adding charles law

--- Gases and liquids/ideal_gas.py
+++ Gases and liquids/ideal_gas.py
@@ -67,3 +67,71 @@ class BoyleLaw:
         plt.title('Isotherm of Boyle with k = {}'.format(k))
         plt.grid(True)
         plt.show()
+
+class CharlesLaw:
+    
+    def __init__(self, V1=None, T1=None, V2=None, T2=None, k=None):
+        self.V1 = V1
+        self.T1 = T1
+        self.V2 = V2
+        self.T2 = T2
+        self.k = k
+    
+    def calculate(self):
+        if self.k:
+            if self.V1:
+                self.T1 = self.k / self.V1
+                return self.T1
+            elif self.T1:
+                self.V1 = self.k / self.T1
+                return self.V1
+        else:
+            # If V1, T1 and V2 are known -> calculate T2
+            if self.V1 is not None and self.T1 is not None and self.V2 is not None and self.T2 is None:
+                self.T2 = (self.V2 * self.T1) / self.V1
+                return self.T2
+            
+            # If V1, T1 and T2 are known -> calculate V2
+            elif self.V1 is not None and self.T1 is not None and self.V2 is None and self.T2 is not None:
+                self.V2 = (self.V1 * self.T2) / self.T1
+                return self.V2
+            
+            # If V2, T1 and T2 are known -> calculate V1
+            elif self.V1 is None and self.T1 is not None and self.V2 is not None and self.T2 is not None:
+                self.V1 = (self.V2 * self.T1) / self.T2
+                return self.V1
+            
+            # If V1, V2 and T2 are known -> calculate T1
+            elif self.V1 is not None and self.V2 is not None and self.T1 is None and self.T2 is not None:
+                self.T1 = (self.V1 * self.T2) / self.V2
+                return self.T1
+            
+            else:
+                raise ValueError("Exactly three values must be provided to calculate the fourth.")
+        
+    def plot_isobar(self, V1, T1, temperature_range):
+        volumes = [(V1 * T) / T1 for T in temperature_range]
+        plt.plot(temperature_range, volumes)
+        plt.xlabel('Temperature (K)')
+        plt.ylabel('Volume (L)')
+        plt.title('Isobar of Charles')
+        plt.grid(True)
+        plt.show()
+
+    def plot_isobar_from_k(self, k, value_range, known='V'):
+        if known == 'V':
+            volumes = value_range
+            temperatures = [k / V for V in volumes]
+        elif known == 'T':
+            temperatures = value_range
+            volumes = [k * T for T in temperatures]
+        else:
+            raise ValueError("'known' should be either 'V' or 'T'")
+        
+        plt.plot(temperatures, volumes)
+        plt.xlabel('Temperature (K)')
+        plt.ylabel('Volume (L)')
+        plt.title('Isobar of Charles with k = {}'.format(k))
+        plt.grid(True)
+        plt.show()
+
--- Gases and liquids/tests/test_ideal_gas.py
+++ Gases and liquids/tests/test_ideal_gas.py
@@ -1,5 +1,6 @@
 import unittest
 from ideal_gas import BoyleLaw
+from ideal_gas import CharlesLaw
 
 class TestBoyleLaw(unittest.TestCase):
 
@@ -59,7 +60,66 @@ class TestBoyleLaw(unittest.TestCase):
         except Exception as e:
             passed = False
         self.assertTrue(passed)
-#c
+
+class TestCharlesLaw(unittest.TestCase):
+
+    def setUp(self):
+        self.charles = CharlesLaw()
+
+    def test_calculate_with_known_k_and_V1(self):
+        self.charles.k = 22.4
+        self.charles.V1 = 22.4
+        T1 = self.charles.calculate()
+        self.assertAlmostEqual(T1, 1)
+
+    def test_calculate_with_known_k_and_T1(self):
+        self.charles.k = 22.4
+        self.charles.T1 = 1
+        V1 = self.charles.calculate()
+        self.assertAlmostEqual(V1, 22.4)
+
+    def test_calculate_for_known_V1_T1_V2(self):
+        self.charles.V1, self.charles.T1, self.charles.V2 = 22.4, 1, 44.8
+        T2 = self.charles.calculate()
+        self.assertAlmostEqual(T2, 2)
+
+    def test_calculate_for_known_V1_T1_T2(self):
+        self.charles.V1, self.charles.T1, self.charles.T2 = 22.4, 1, 2
+        V2 = self.charles.calculate()
+        self.assertAlmostEqual(V2, 44.8)
+
+    def test_calculate_for_known_V2_T1_T2(self):
+        self.charles.V2, self.charles.T1, self.charles.T2 = 44.8, 1, 2
+        V1 = self.charles.calculate()
+        self.assertAlmostEqual(V1, 22.4)
+
+    def test_calculate_for_known_V1_V2_T2(self):
+        self.charles.V1, self.charles.V2, self.charles.T2 = 22.4, 44.8, 2
+        T1 = self.charles.calculate()
+        self.assertAlmostEqual(T1, 1)
+
+    def test_incorrect_parameters(self):
+        with self.assertRaises(ValueError):
+            self.charles.calculate()
+
+    def test_plot_isobar(self):
+        # This test ensures the plotting function runs without errors
+        try:
+            self.charles.plot_isobar(22.4, 1, range(1, 11))
+            passed = True
+        except Exception as e:
+            passed = False
+        self.assertTrue(passed)
+
+    def test_plot_isobar_from_k(self):
+        # This test ensures the plotting function runs without errors
+        try:
+            self.charles.plot_isobar_from_k(22.4, range(1, 11), known='V')
+            passed = True
+        except Exception as e:
+            passed = False
+        self.assertTrue(passed)
+
 # Run the tests
 if __name__ == '__main__':
     unittest.main()
