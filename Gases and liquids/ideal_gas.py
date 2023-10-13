cf478ee54a1cebf24db3b76b055900bbf57dee3a
gabacame <ga.bacame@gmail.com>
2023-10-13 12:27:29 -0700

    adding k calculation to ideal_gas.py and implement testing

--- Gases and liquids/ideal_gas.py
+++ Gases and liquids/ideal_gas.py
@@ -1,32 +1,83 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
 class BoyleLaw:
     
-    def __init__(self, P1=None, V1=None, P2=None, V2=None):
+    def __init__(self, P1=None, V1=None, P2=None, V2=None, k=None):
         self.P1 = P1
         self.V1 = V1
         self.P2 = P2
         self.V2 = V2
+        self.k = k
     
     def calculate(self):
-        # If P1, V1 and P2 are known -> calculate V2
-        if self.P1 is not None and self.V1 is not None and self.P2 is not None and self.V2 is None:
-            self.V2 = (self.P1 * self.V1) / self.P2
-            return self.V2
-        
-        # If P1, V1 and V2 are known -> calculate P2
-        elif self.P1 is not None and self.V1 is not None and self.P2 is None and self.V2 is not None:
-            self.P2 = (self.P1 * self.V1) / self.V2
-            return self.P2
-        
-        # If V1, P2 and V2 are known -> calculate P1
-        elif self.P1 is None and self.V1 is not None and self.P2 is not None and self.V2 is not None:
-            self.P1 = (self.P2 * self.V2) / self.V1
-            return self.P1
-        
-        # If P1, P2 and V2 are known -> calculate V1
-        elif self.P1 is not None and self.V1 is None and self.P2 is not None and self.V2 is not None:
-            self.V1 = (self.P2 * self.V2) / self.P1
-            return self.V1
-        
+        if self.k:
+            if self.P1:
+                self.V1 = self.k / self.P1
+                return self.V1
+            elif self.V1:
+                self.P1 = self.k / self.V1
+                return self.P1
         else:
-            raise ValueError("Exactly three values must be provided to calculate the fourth.")
+            # If P1, V1 and P2 are known -> calculate V2
+            if self.P1 is not None and self.V1 is not None and self.P2 is not None and self.V2 is None:
+                self.V2 = (self.P1 * self.V1) / self.P2
+                return self.V2
+            
+            # If P1, V1 and V2 are known -> calculate P2
+            elif self.P1 is not None and self.V1 is not None and self.P2 is None and self.V2 is not None:
+                self.P2 = (self.P1 * self.V1) / self.V2
+                return self.P2
+            
+            # If V1, P2 and V2 are known -> calculate P1
+            elif self.P1 is None and self.V1 is not None and self.P2 is not None and self.V2 is not None:
+                self.P1 = (self.P2 * self.V2) / self.V1
+                return self.P1
+            
+            # If P1, P2 and V2 are known -> calculate V1
+            elif self.P1 is not None and self.V1 is None and self.P2 is not None and self.V2 is not None:
+                self.V1 = (self.P2 * self.V2) / self.P1
+                return self.V1
+            
+            else:
+                raise ValueError("Exactly three values must be provided to calculate the fourth.")
+        
+    def plot_isotherm(self, P1, V1, pressure_range):
+        volumes = [(P1 * V1) / P for P in pressure_range]
+        plt.plot(volumes, pressure_range)
+        plt.xlabel('Volume (L)')
+        plt.ylabel('Pressure (atm)')
+        plt.title('Isotherm of Boyle')
+        plt.grid(True)
+        plt.show()
 
+    def plot_isotherm_from_k(self, k, value_range, known='P'):
+        if known == 'P':
+            pressures = value_range
+            volumes = [k / P for P in pressures]
+        elif known == 'V':
+            volumes = value_range
+            pressures = [k / V for V in volumes]
+        else:
+            raise ValueError("'known' should be either 'P' or 'V'")
+        
+        plt.plot(volumes, pressures)
+        plt.xlabel('Volume (L)')
+        plt.ylabel('Pressure (atm)')
+        plt.title('Isotherm of Boyle with k = {}'.format(k))
+        plt.grid(True)
+        plt.show()
+
+# Using the BoyleLaw class to generate and plot an isotherm
+P_initial = 1  #Initial pressure (e.g. 1 atm) # Initial pressure (e.g. 1 atm)
+V_initial = 22.4  # Standard molar volume for ideal gases at 0°C and 1 atm
+pressures = np.linspace(0.1, 10, 400)  # 
+
+boyle = BoyleLaw()
+boyle.plot_isotherm(P_initial, V_initial, pressures)
+
+k_value = 22.4  # k = P * V
+pressure_range = np.linspace(0.1, 10, 400)  # A range of pressures from 0.1 to 10 atm
+
+boyle = BoyleLaw(k=k_value)
+boyle.plot_isotherm_from_k(k_value, pressure_range, known='P')
---
+++ Gases and liquids/tests/test_ideal_gas.py
@@ -0,0 +1,62 @@
+import unittest
+from ideal_gas import BoyleLaw
+
+class TestBoyleLaw(unittest.TestCase):
+
+    def setUp(self):
+        self.boyle = BoyleLaw()
+
+    def test_calculate_with_known_k_and_P1(self):
+        self.boyle.k = 22.4
+        self.boyle.P1 = 1
+        V1 = self.boyle.calculate()
+        self.assertAlmostEqual(V1, 22.4)
+
+    def test_calculate_with_known_k_and_V1(self):
+        self.boyle.k = 22.4
+        self.boyle.V1 = 22.4
+        P1 = self.boyle.calculate()
+        self.assertAlmostEqual(P1, 1)
+
+    def test_calculate_for_known_P1_V1_P2(self):
+        self.boyle.P1, self.boyle.V1, self.boyle.P2 = 1, 22.4, 2
+        V2 = self.boyle.calculate()
+        self.assertAlmostEqual(V2, 11.2)
+
+    def test_calculate_for_known_P1_V1_V2(self):
+        self.boyle.P1, self.boyle.V1, self.boyle.V2 = 1, 22.4, 11.2
+        P2 = self.boyle.calculate()
+        self.assertAlmostEqual(P2, 2)
+
+    def test_calculate_for_known_V1_P2_V2(self):
+        self.boyle.V1, self.boyle.P2, self.boyle.V2 = 22.4, 2, 11.2
+        P1 = self.boyle.calculate()
+        self.assertAlmostEqual(P1, 1)
+
+    def test_calculate_for_known_P1_P2_V2(self):
+        self.boyle.P1, self.boyle.P2, self.boyle.V2 = 1, 2, 11.2
+        V1 = self.boyle.calculate()
+        self.assertAlmostEqual(V1, 22.4)
+
+    def test_incorrect_parameters(self):
+        with self.assertRaises(ValueError):
+            self.boyle.calculate()
+
+    def test_plot_isotherm(self):
+        # This test ensures the plotting function runs without errors
+        try:
+            self.boyle.plot_isotherm(1, 22.4, range(1, 11))
+            passed = True
+        except Exception as e:
+            passed = False
+        self.assertTrue(passed)
+
+    def test_plot_isotherm_from_k(self):
+        # This test ensures the plotting function runs without errors
+        try:
+            self.boyle.plot_isotherm_from_k(22.4, range(1, 11), known='P')
+            passed = True
+        except Exception as e:
+            passed = False
+        self.assertTrue(passed)
+
